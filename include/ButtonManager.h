2ba59595fadd672c7263b7af69deb2d807378318
keith8496 <ksenti@gmail.com>
2025-11-28 13:14:15 -0600

    cleanup

--- include/ButtonManager.h
+++ include/ButtonManager.h
@@ -2,6 +2,8 @@
 
 #include <stdint.h>
 
+constexpr const uint32_t LONG_PRESS_DEFAULT_MS = 500;
+
 // Which button generated the event
 enum class ButtonID : uint8_t {
     None = 0,
@@ -26,14 +28,14 @@ struct ButtonEvent {
 class ButtonManager {
 public:
     // Configure how long (in ms) a press must be held to count as "long"
-    void begin(uint32_t longPressMs = 600);
+    void begin(uint32_t longPressMs = LONG_PRESS_DEFAULT_MS);
 
     // Call once per loop *after* M5.update().
     // Returns ButtonEvent with type==None when no event.
     ButtonEvent poll();
 
 private:
-    uint32_t _longPressMs = 600;
+    uint32_t _longPressMs = LONG_PRESS_DEFAULT_MS;
 
     // Simple state for A
     bool     _aWasDown    = false;
