07d4f93e4fd7b1f322d388a447641e4deef53872
keith8496 <ksenti@gmail.com>
2025-11-28 19:12:08 -0600

    Fix NTP

--- include/NetworkModule.h
+++ include/NetworkModule.h
@@ -9,3 +9,12 @@ extern WiFiManager wm;
 
 void WiFi_setup();
 void WiFi_onLoop();
+void WiFi_onEvent(WiFiEvent_t event);
+void WiFi_onSaveParams();
+
+// Debounced time initialization API
+void requestTimeInit();
+void serviceTimeInit();
+
+// Optional: request a one-shot NTP resync via MQTT /cmd
+void requestTimeResync();
